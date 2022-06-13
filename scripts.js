f5308cea1a300a16d672dd2130121f455b2f099c
JaelAguilar <71282327+JaelAguilar@users.noreply.github.com>
2022-06-13 10:56:11 -0500

    Added documentation

--- scripts.js
+++ scripts.js
@@ -1,17 +1,25 @@
 let numbers = [...document.getElementsByClassName("number")]
 let display = document.getElementById("screen")
-let previousValue="",currentValue="",sign
+let previousValue="",currentValue="",globalSign
 
 numbers.forEach(number => {
     number.addEventListener("click", () => insertValue(number.textContent))
 });
 
+/**
+ * Function to insert values, changes the currentValue and shows it on the screen.
+ * @param {String} digit 
+ */
 function insertValue(digit) {
     currentValue += digit
     display.innerText=currentValue
 }
 
-function operation(sign2) {
+/**
+ * Changes the type of operation used depneding on the button pressed. If two values were already inserted, it shows the result, else it updates the previous and current value
+ * @param {String} newSign 
+ */
+function operation(newSign) {
     if (previousValue !== "") {
         result=showResult()
         previousValue = result
@@ -21,14 +29,18 @@ function operation(sign2) {
         previousValue = currentValue
         currentValue=""
     }
-    sign=sign2
+    globalSign=newSign
 }
 
+/**
+ * Makes the operation and shows it on the screen
+ * @returns result of operation
+ */
 function showResult(){
     let result
     previousValue = parseInt(previousValue)
     currentValue=parseInt(currentValue)
-    switch (sign) {
+    switch (globalSign) {
         case "+":
             result=previousValue+currentValue
             break;
