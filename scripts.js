9a873cf43f17ccda6f12b0000629074d6cab0dea
JaelAguilar <71282327+JaelAguilar@users.noreply.github.com>
2022-06-12 20:50:43 -0500

    Basic operation functionality

--- scripts.js
+++ scripts.js
@@ -1,9 +1,50 @@
 let numbers = [...document.getElementsByClassName("number")]
-console.log(numbers.textContent)
-let previousValue,currentValue=""
+let display = document.getElementById("screen")
+let previousValue="",currentValue="",sign
 
 numbers.forEach(number => {
-
-    number.addEventListener("click",()=>console.log(number.textContent))
+    number.addEventListener("click", () => insertValue(number.textContent))
 });
 
+function insertValue(digit) {
+    console.log("PreviousValue: "+previousValue)
+    currentValue += digit
+    console.log("Current Value: "+currentValue)
+}
+
+function operation(sign2) {
+    if (previousValue !== "") {
+        result=showResult()
+        previousValue = result
+        currentValue=""
+    }
+    else {
+        previousValue = currentValue
+        currentValue=""
+    }
+    sign=sign2
+}
+
+function showResult(){
+    let result
+    previousValue = parseInt(previousValue)
+    currentValue=parseInt(currentValue)
+    switch (sign) {
+        case "+":
+            result=previousValue+currentValue
+            break;
+        case "-":
+            result=previousValue-currentValue
+            break;
+        case "*":
+            result=previousValue*currentValue
+            break;
+        case "/":
+            result=previousValue/currentValue
+            break;
+        default:
+            break;
+    }
+    display.innerText=result
+    return result
+}
